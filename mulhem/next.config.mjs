fe8457437e31b0b7e63e214165f54d5299977ea4
Hessah Alharbi <108225318+Hessa-Alharbi@users.noreply.github.com>
2025-05-08 14:23:05 +0300

    Update next.config.mjs

--- mulhem/next.config.mjs
+++ mulhem/next.config.mjs
@@ -14,8 +14,15 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@heroicons/react/24/outline': false,
+      '@heroicons/react/24/solid': false,
+    };
+
+    return config;
+  },
 }
-
-
 
 export default nextConfig
